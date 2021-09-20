84e058cfd3264d6632cf7619d77354dcca53a797
RohitashYogi <rohitash.yogi@probusinsurance.com>
2021-09-20 17:51:11 +0530

    Initial commit

---
+++ app/src/main/java/com/Yogify/bottomsheetlibrary/MainActivity.java
@@ -0,0 +1,18 @@
+package com.Yogify.bottomsheetlibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.yogify.bottomsheetdialog.ShowBottomsheet;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ShowBottomsheet.ShowBottomSheet(MainActivity.this,R.layout.bottomsheet,R.id.relativelayout);
+    }
+}
