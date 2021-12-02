68595d0dca10701c3c81e28cbbf7ffafa5563d17
RohitashYogi <rohitash.yogi@probusinsurance.com>
2021-12-02 10:38:17 +0530

    Bottom Sheet Try To Make Libarary

--- app/src/main/java/com/Yogify/bottomsheetlibrary/MainActivity.java
+++ app/src/main/java/com/Yogify/bottomsheetlibrary/MainActivity.java
@@ -5,17 +5,84 @@ import static com.yogify.bottomsheetdialog.ShowBottomsheet.ShowActivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.navigationrail.NavigationRailMenuView;
+import com.google.android.material.navigationrail.NavigationRailView;
 import com.yogify.bottomsheetdialog.ShowBottomsheet;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ShowBottomsheet.ShowBottomSheet(MainActivity.this,R.layout.bottomsheet,R.id.relativelayout);
-        ShowActivity(MainActivity.this);
+
+        NaviagitonRails();
+
+        // ShowBottomsheet.ShowBottomSheet(MainActivity.this,R.layout.bottomsheet,R.id.relativelayout);
+        // ShowActivity(MainActivity.this);
+        // MatirialARangPicker();
+        MatrialDatePicker();
     }
+
+    private void NaviagitonRails() {
+        NavigationRailView navigationRailView;
+        BadgeDrawable badge;
+        navigationRailView = findViewById(R.id.navigation_rail);
+
+        badge = navigationRailView.getOrCreateBadge(R.id.alarms);
+        badge.setVisible(true);
+        badge.setNumber(33);
+
+
+        badge = navigationRailView.getBadge(R.id.alarms);
+        if (badge != null) {
+            badge.setVisible(false);
+            badge.clearNumber();
+        }
+
+        navigationRailView.removeBadge(R.id.alarms);
+
+    }
+
+    public void MatirialARangPicker() {
+        MaterialDatePicker dateRangePicker =
+                MaterialDatePicker.Builder.dateRangePicker()
+                        .setTitleText("Select dates")
+                        .build();
+        dateRangePicker.show(getSupportFragmentManager(), "This");
+        dateRangePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+            @Override
+            public void onPositiveButtonClick(Object selection) {
+                Toast.makeText(getApplicationContext(), selection.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public void MatrialDatePicker() {
+        MaterialDatePicker datePicker =
+                MaterialDatePicker.Builder.datePicker()
+                        .setTitleText("Select date")
+                        .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                        .build();
+
+        datePicker.show(getSupportFragmentManager(), "Hell dATA");
+        datePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+            @Override
+            public void onPositiveButtonClick(Object selection) {
+                Toast.makeText(getApplicationContext(), selection.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
 }
