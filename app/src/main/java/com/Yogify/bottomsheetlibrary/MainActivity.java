4a2ef790b83f9df25fd5c1ebefe599f7304d01f5
RohitashYogi <rohitash.yogi@probusinsurance.com>
2022-02-10 18:15:31 +0530

    Android Library With Custom View Is Used

--- app/src/main/java/com/Yogify/bottomsheetlibrary/MainActivity.java
+++ app/src/main/java/com/Yogify/bottomsheetlibrary/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         MatrialYou.ShowMatrialDatePicker(getApplicationContext(), getSupportFragmentManager());
 
+
     }
 
 
